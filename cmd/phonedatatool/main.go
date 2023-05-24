3953ac8c899b4bd70826e83284b897f4c008f010
jyh <jyh@eastcom.com>
2023-05-24 13:44:02 +0800

    version 0.1.0

--- cmd/phonedatatool/main.go
+++ cmd/phonedatatool/main.go
@@ -15,7 +15,7 @@ import (
 
 const (
 	Name     = "phonedatatool"
-	Version  = "0.0.1"
+	Version  = "0.1.0"
 	Author   = "seedjyh@gmail.com"
 	FullName = Name + "_" + Version + "(" + Author + ")"
 )
