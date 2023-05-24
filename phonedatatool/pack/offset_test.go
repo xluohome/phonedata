54a70b66b5865603e2cbb73cc4c882ab11bb55f4
jyh <jyh@eastcom.com>
2023-05-24 15:34:29 +0800

    开发新的 offset parse

--- phonedatatool/pack/offset_test.go
+++ phonedatatool/pack/offset_test.go
@@ -1,8 +1,17 @@
 package pack
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 import "github.com/stretchr/testify/assert"
 
 func TestOffset_Bytes(t *testing.T) {
 	assert.Equal(t, []byte{0xd2, 0x04, 0x00, 0x00}, Offset(1234).Bytes())
+}
+
+func TestOffset_Parse(t *testing.T) {
+	offset := new(Offset)
+	assert.NoError(t, offset.Parse(bytes.NewReader([]byte{0xd2, 0x04, 0x00, 0x00})))
+	assert.Equal(t, Offset(1234), *offset)
 }
